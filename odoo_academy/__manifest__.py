24445dd6b135c11fc6697db75cf7bca337dcee3b
PCA <pca@odoo.com>
2021-08-02 20:24:49 +0000

    academy, initial commit

---
+++ odoo_academy/__manifest__.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': 'Odoo Academy',
+    'summary': """Academy app to manage Training""",
+    'description': """
+        Academy Module to manage Training:
+        - Courses
+        - Sessions
+        -Attendees
+    """,
+    
+    'author': 'PCA',
+    'website': 'https://www.odoo.com',,
+    'category': 'training',
+    'version': '0.1',
+    
+    'depends': ['base'],
+    'data': [
+    
+        
+    ],
+    'demo': [
+        
+        
+    ],
+}
