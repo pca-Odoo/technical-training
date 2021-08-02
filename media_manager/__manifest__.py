13b5df20af06e9f3fc58bd55984f303e4ccaf8a2
PCA <pca@odoo.com>
2021-08-02 21:13:38 +0000

    media, initial commit

---
+++ media_manager/__manifest__.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': 'Media Manager',
+    'summary': """App to entertainment media""",
+    'description': """
+        Media module to manage:
+        - Movies
+        - Shows
+        - Songs
+    """,
+    
+    'author': 'PCA',
+    'website': 'https://www.odoo.com',
+    'category': 'training',
+    'version': '0.1',
+    
+    'depends': ['base'],
+    'data': [
+    
+        
+    ],
+    'demo': [
+        
+        
+    ],
+}
