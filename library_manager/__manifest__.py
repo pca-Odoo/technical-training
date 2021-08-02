10f20b4d6247455aeac79a35c845986eee8e5608
PCA <pca@odoo.com>
2021-08-02 20:52:31 +0000

    library, initial commit

---
+++ library_manager/__manifest__.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': 'Library Manager',
+    'summary': """Library app to manage all library needs""",
+    'description': """
+        Manage all aspects of your library:
+        - Books
+        - Memberships
+        - Rentals
+    """,
+    
+    'author': 'PCA',
+    'website': 'https://www.odoo.com',
+    'category': 'training',
+    'version': '0.1',
+    
+    'depends': ['base'],
+    'data': [
+        
+        
+    ],
+    'demo': [
+        
+        
+    ],
+}
