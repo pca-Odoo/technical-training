2c62bc3feb899d40b4c65409b15ebdbf344c1697
PCA <pca@odoo.com>
2021-08-02 22:20:33 +0000

    books fields added

--- library_manager/models/books.py
+++ library_manager/models/books.py
@@ -6,3 +6,15 @@ class Books(models.Model):
     
     _name = 'library.book'
     _description = 'Book Info'
+    
+    name = fields.Char(string='Title', required=True)
+    description = fields.Text(string='Description')
+    
+    authors = fields.Text(string='Author(s)',
+                          required=True,
+                          help='Author of book, can be multiple')
+    editors = fields.Text(string='Editor(s)')
+    publisher = fields.Char(string='Publisher')
+    edition_year = fields.Integer(string='Year of Edition')
+    isbn = fields.Integer(string='ISBN')
+    genre = fields.Char(string="Genre", required=True)
