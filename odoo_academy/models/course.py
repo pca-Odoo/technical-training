e4fd9730b7c01ca21184f6116319de47495fb09f
PCA <pca@odoo.com>
2021-08-04 23:41:09 +0000

    added session model and related fields to course

--- odoo_academy/models/course.py
+++ odoo_academy/models/course.py
@@ -23,6 +23,10 @@ class Course(models.Model):
     additional_fee = fields.Float(string='Additional Fee', default=0.00)
     total_price = fields.Float(string='Total Price', readonly=True)
     
+    session_ids = fields.One2many(comodel_name='academy.session',
+                                 inverse_name='course_id',
+                                 string='Sessions')
+    
     @api.onchange('base_price', 'additional_fee')
     def _onchange_total_price(self):
         if self.base_price < 0.00:
